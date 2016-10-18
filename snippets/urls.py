2e5449a3208411892c9269e7c5a7fa3b3d2a993e
crashtack <crashtack@gmail.com>
2016-10-18 15:40:20 -0700

    Finished Authentication and Permissions

--- snippets/urls.py
+++ snippets/urls.py
@@ -5,6 +5,8 @@ from rest_framework.urlpatterns import format_suffix_patterns
 urlpatterns = [
     url(r'^snippets/$', views.SnippetList.as_view()),
     url(r'^snippets/(?P<id>[0-9]+)/$', views.SnippetList.as_view()),
+    url(r'^users/$', views.UserList.as_view()),
+    url(r'^users/(?P<id>[0-9]+)/$', views.UserDetail.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
