dd9c17d14ed31ed254d676e93ca7656c0df07832
crashtack <crashtack@gmail.com>
2016-10-19 09:23:15 -0700

    Binding ViewSets to URLs explicitly

--- snippets/urls.py
+++ snippets/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import url, include
 from snippets import views
 from rest_framework.urlpatterns import format_suffix_patterns
-from snippet.views import SnippetViewSet, UserViewSet, api_root
+from snippets.views import SnippetViewSet, UserViewSet, api_root
 from rest_framework import renderers
 
 
